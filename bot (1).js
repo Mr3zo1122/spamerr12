f8c15caa832467cbdb4232d52467845ba790a7e8
Mr3zo1122 <45174810+Mr3zo1122@users.noreply.github.com>
2018-11-19 18:38:39 +0300

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "514099886023507968"; // ايدي السررفر
+var channel = "514099886023507970";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
